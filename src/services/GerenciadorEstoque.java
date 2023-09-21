f2d0ce943a1ba7d84d20cf065d8c9ab545ac8311
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-09-20 23:36:40 -0300

    criando uma interfaca para salvar as listas

---
+++ src/services/GerenciadorEstoque.java
@@ -0,0 +1,29 @@
+package services;
+
+import model.Produto;
+import model.Unidade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface GerenciadorEstoque {
+    List<Unidade> unidades = new ArrayList<>();
+
+    List<Produto> produtos = new ArrayList<>();
+
+
+    public  static void preencheUnidade() {
+        unidades.add(new Unidade("unidade", "un"));
+        unidades.add(new Unidade("exemplares", "exs"));
+        unidades.add(new Unidade("kilo", "kg"));
+        unidades.add(new Unidade("pacote", "pct"));
+    }
+
+    public static void preencheProduto() {
+        produtos.add(new Produto("café", 23, GerenciadorUnidade.encontrarUnidadePorId(1)));
+        produtos.add(new Produto("arroz", 34, GerenciadorUnidade.encontrarUnidadePorId(2)));
+        produtos.add(new Produto("açucar", 56, GerenciadorUnidade.encontrarUnidadePorId(3)));
+        produtos.add(new Produto("sal", 89, GerenciadorUnidade.encontrarUnidadePorId(4)));
+    }
+
+}
