89ba065ab858f279b585ee1c0570e4bfd0cad23a
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-09-20 23:37:23 -0300

    criando uma classe para menu unidade

---
+++ src/menu/MenuUnidade.java
@@ -0,0 +1,56 @@
+package menu;
+
+import model.Unidade;
+import services.GerenciadorEstoque;
+import services.GerenciadorUnidade;
+
+import java.util.Scanner;
+
+public class MenuUnidade {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void menuUnidade() {
+        boolean sair = false;
+
+        while (!sair) {
+            exibirMenuUnidade();
+            int opcao = scanner.nextInt();
+            scanner.nextLine(); // Limpar o buffer do scanner
+
+            switch (opcao) {
+                case 1:
+                    GerenciadorUnidade.adicionarUnidade();
+                    break;
+                case 2:
+                    GerenciadorUnidade.listarUnidades();
+                    break;
+                case 3:
+                    GerenciadorUnidade.atualizarUnidade();
+                    break;
+                case 4:
+                    GerenciadorUnidade.deletarUnidade();
+                    break;
+                case 5:
+                    sair = true;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+
+    public static void exibirMenuUnidade() {
+        System.out.println("\n--- Menu Unidade ---");
+        System.out.println("1 - Adicionar Unidade");
+        System.out.println("2 - Listar Unidades");
+        System.out.println("3 - Atualizar Unidade");
+        System.out.println("4 - Deletar Unidade");
+        System.out.println("5 - Voltar ao menu principal");
+        System.out.print("Escolha uma opção: ");
+    }
+}
+
+
+
