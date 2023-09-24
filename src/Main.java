65f8048b853f46e11217f9035ee6be9539cfa242
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-09-24 16:36:15 -0300

    corrigindo alguns problemas de códigos

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Main {
 
 
-        private static Scanner scanner = new Scanner(System.in);
+        private static final Scanner scanner = new Scanner(System.in);
 
         public static void main(String[] args) {
 
--- src/services/GerenciadorEstoque.java
+++ src/services/GerenciadorEstoque.java
@@ -16,21 +16,21 @@ public interface GerenciadorEstoque {
     List<Movimentacao> movimentacoes = new ArrayList<>();
 
 
-    public  static void preencheUnidade() {
+    static void preencheUnidade() {
         unidades.add(new Unidade("unidade", "un"));
         unidades.add(new Unidade("exemplares", "exs"));
         unidades.add(new Unidade("kilo", "kg"));
         unidades.add(new Unidade("pacote", "pct"));
     }
 
-    public static void preencheProduto() {
+    static void preencheProduto() {
         produtos.add(new Produto("café",   GerenciadorUnidade.encontrarUnidadePorId(1)));
         produtos.add(new Produto("arroz",   GerenciadorUnidade.encontrarUnidadePorId(2)));
         produtos.add(new Produto("açucar",  GerenciadorUnidade.encontrarUnidadePorId(3)));
         produtos.add(new Produto("sal",   GerenciadorUnidade.encontrarUnidadePorId(4)));
     }
 
-    public static void preencheMovimento() {
+    static void preencheMovimento() {
         movimentacoes.add(new Movimentacao(TipoMovimento.ENTRADA,   GerenciadorProduto.encontrarProdutoPorId(1),159));
         movimentacoes.add(new Movimentacao(TipoMovimento.SAIDA,   GerenciadorProduto.encontrarProdutoPorId(1),98));
         movimentacoes.add(new Movimentacao(TipoMovimento.ENTRADA,   GerenciadorProduto.encontrarProdutoPorId(2),900));
