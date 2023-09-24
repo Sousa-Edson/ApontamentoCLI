8293909a64edbbf70d90f0cb63973813630d06de
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-09-24 16:26:56 -0300

    criandp classe meu saldo

---
+++ src/menu/MenuSaldo.java
@@ -0,0 +1,37 @@
+package menu;
+
+import services.GerenciadorMovimentacao;
+import services.GerenciadorProduto;
+
+import java.util.Scanner;
+
+public class MenuSaldo {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void menuSaldo() {
+        boolean sair = false;
+        int codigoProduto = 0;
+        while (!sair) {
+            System.out.print("Digite o id do produto para saldo: ");
+            codigoProduto = scanner.nextInt();
+            if (GerenciadorProduto.encontrarProdutoPorId(codigoProduto) != null) {
+                GerenciadorMovimentacao.saldoProduto(codigoProduto);
+                System.out.println("");
+
+                    sair = true;
+
+            } else {
+                System.out.println("*** Produto não encontrado! ***");
+                sair = false;
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
