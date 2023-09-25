02f4e611c1d0ea4772d8f34057605825e0d7ae93
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-09-25 00:06:45 -0300

    bugfix: corrigindo sql unidades

--- src/sql/unidades.sql
+++ src/sql/unidades.sql
@@ -1,8 +1,8 @@
--- apontamento1.Unidade definition
+-- apontamento1.unidades definition
 
 CREATE TABLE `unidades` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `codigo` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   `sigla` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
